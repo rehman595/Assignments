b024e6fc62f2520f9808cf09010e89bbb880e556
rehman595 <115399540+rehman595@users.noreply.github.com>
2022-10-16 21:03:28 +0500

    Add files via upload

---
+++ oddeven/EvenODD.java
@@ -0,0 +1,16 @@
+public class EvenODD {
+    public static void main (String[] args)
+    {
+        int number = 56;
+
+        if(number%2==0)
+        {
+            System.out.println("Even Number");
+        }
+
+        else {
+            System.out.println("ODD Number");
+        }
+    }
+    
+}
